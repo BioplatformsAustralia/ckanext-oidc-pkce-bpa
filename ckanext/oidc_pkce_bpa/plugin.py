fbd6afe7c5334a0c039586cd4bc2103787939811
aai-dev <ubuntu@aai-demo-ckan.novalocal>
2025-07-04 00:57:03 +0000

    test simple import

--- ckanext/oidc_pkce_bpa/plugin.py
+++ ckanext/oidc_pkce_bpa/plugin.py
@@ -1,32 +1,34 @@
-from ckanext.oidc_pkce.plugin import OIDCPkcePlugin
-from ckan.plugins import SingletonPlugin
-import urllib.parse
-from flask import request
-import ckan.plugins.toolkit as toolkit
+import ckanext.oidc_pkce.plugin
 
-class OIDCPkceBpaPlugin(OIDCPkcePlugin, SingletonPlugin):
-    """
-    BPA extension plugin extending OIDCPkcePlugin to support Auth0 extra params (e.g., audience, scope).
-    """
+# from ckanext.oidc_pkce.plugin import OIDCPkcePlugin
+# from ckan.plugins import SingletonPlugin
+# import urllib.parse
+# from flask import request
+# import ckan.plugins.toolkit as toolkit
 
-    def _get_auth0_extra_params(self):
-        config = toolkit.config
-        param_string = config.get('ckanext.oidc.pkce.auth0_extra_params', '')
-        param_pairs = [param.split('=') for param in param_string.split('&') if '=' in param]
-        params = {k: v for k, v in param_pairs}
-        return params
+# class OIDCPkceBpaPlugin(OIDCPkcePlugin, SingletonPlugin):
+#     """
+#     BPA extension plugin extending OIDCPkcePlugin to support Auth0 extra params (e.g., audience, scope).
+#     """
 
-    def login_redirect(self):
-        # Call original method first
-        url = super(OIDCPkceBpaPlugin, self).login_redirect()
+#     def _get_auth0_extra_params(self):
+#         config = toolkit.config
+#         param_string = config.get('ckanext.oidc.pkce.auth0_extra_params', '')
+#         param_pairs = [param.split('=') for param in param_string.split('&') if '=' in param]
+#         params = {k: v for k, v in param_pairs}
+#         return params
 
-        # Add extra params
-        extra_params = self._get_auth0_extra_params()
-        if extra_params:
-            parsed = urllib.parse.urlparse(url)
-            query = dict(urllib.parse.parse_qsl(parsed.query))
-            query.update(extra_params)
-            new_query = urllib.parse.urlencode(query)
-            url = urllib.parse.urlunparse(parsed._replace(query=new_query))
+#     def login_redirect(self):
+#         # Call original method first
+#         url = super(OIDCPkceBpaPlugin, self).login_redirect()
 
-        return url
+#         # Add extra params
+#         extra_params = self._get_auth0_extra_params()
+#         if extra_params:
+#             parsed = urllib.parse.urlparse(url)
+#             query = dict(urllib.parse.parse_qsl(parsed.query))
+#             query.update(extra_params)
+#             new_query = urllib.parse.urlencode(query)
+#             url = urllib.parse.urlunparse(parsed._replace(query=new_query))
+
+#         return url
