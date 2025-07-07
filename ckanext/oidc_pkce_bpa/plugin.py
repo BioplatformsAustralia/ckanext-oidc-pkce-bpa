e2add90cc805cfb2f762df5d360ed20c69b1d012
aai-dev <ubuntu@aai-demo-ckan.novalocal>
2025-07-07 01:26:14 +0000

    initial working status of ckanext-oidc-pkce-bpa

--- ckanext/oidc_pkce_bpa/plugin.py
+++ ckanext/oidc_pkce_bpa/plugin.py
@@ -1,32 +1,18 @@
-from ckanext.oidc_pkce.plugin import OidcPkcePlugin
-from ckan.plugins import SingletonPlugin
-import urllib.parse
-from flask import request
+import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+from ckanext.oidc_pkce.plugin import OidcPkcePlugin
+from ckan.plugins import SingletonPlugin
+
 class OidcPkceBpaPlugin(OidcPkcePlugin, SingletonPlugin):
-    """
-    BPA extension plugin extending OIDCPkcePlugin to support Auth0 extra params (e.g., audience, scope).
-    """
+    plugins.implements(plugins.IConfigurer)
+    
 
-    def _get_auth0_extra_params(self):
-        config = toolkit.config
-        param_string = config.get('ckanext.oidc.pkce.auth0_extra_params', '')
-        param_pairs = [param.split('=') for param in param_string.split('&') if '=' in param]
-        params = {k: v for k, v in param_pairs}
-        return params
+    # IConfigurer
 
-    def login_redirect(self):
-        # Call original method first
-        url = super(OidcPkceBpaPlugin, self).login_redirect()
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, "templates")
+        toolkit.add_public_directory(config_, "public")
+        toolkit.add_resource("assets", "oidc_pkce_bpa")
 
-        # Add extra params
-        extra_params = self._get_auth0_extra_params()
-        if extra_params:
-            parsed = urllib.parse.urlparse(url)
-            query = dict(urllib.parse.parse_qsl(parsed.query))
-            query.update(extra_params)
-            new_query = urllib.parse.urlencode(query)
-            url = urllib.parse.urlunparse(parsed._replace(query=new_query))
-
-        return url
+    
