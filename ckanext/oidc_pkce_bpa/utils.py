be3a1a99880dbd5efb342d9b8767a153590f8338
aai-dev <ubuntu@aai-demo-ckan.novalocal>
2025-09-08 04:46:14 +0000

    get the right username_claim

--- ckanext/oidc_pkce_bpa/utils.py
+++ ckanext/oidc_pkce_bpa/utils.py
@@ -8,7 +8,7 @@ log = logging.getLogger(__name__)
 
 def extract_username(userinfo: dict) -> str:
     username_claim = ckan_config.get("ckanext.oidc_pkce_bpa.username_claim")
-    username = userinfo.get(USERNAME_CLAIM) or userinfo.get("nickname")
+    username = userinfo.get(username_claim) or userinfo.get("nickname")
     if not username:
         raise tk.NotAuthorized("Missing 'username' in Auth0 ID token")
     return username
